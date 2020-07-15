6003dddaff561d17d8b2c6dc19c4c021e31f35e3
sreedhar <sreedharravvari@gmail.com>
2020-07-15 19:52:13 +0530

    Added essential Files.

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 let indicator = 0;
-function info() 
-{
-    if (indicator === 0) 
-    {
-        document.querySelector('#insertHTML').innerHTML = 'You may have noticed that there are not many <b>good</b> photos of mine. Alas!!';
+let element = document.querySelector('#insertHTML');
+function info() {
+    if (indicator === 0) {
+        element.innerHTML = 'You may have noticed that there are not many <b>good</b> photos of mine. Alas!!';
         indicator = 1;
     }
-    else 
-    {
-        document.querySelector('#insertHTML').innerHTML = '';
+    else {
+        element.innerHTML = '';
         indicator = 0;
     }
 }
